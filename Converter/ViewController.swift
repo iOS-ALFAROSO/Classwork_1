070896f8fa98ec832d6e70e313951e509d0a5fd3
Alberth Rodriguez <alberth.sotelo@gmail.com>
2024-09-09 23:05:46 -0700

    Functionality

--- Converter/ViewController.swift
+++ Converter/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempText: UITextField!
+    
+    @IBOutlet weak var resultLable: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ConvertTemp(_ sender: Any) {
+        
+        guard let tempString = tempText.text
+        else{
+            return
+        }
+        
+        if let fahrenheit = Double (tempString){
+            let celsius = (fahrenheit - 32) / 1.8
+            let resultText = "Celsius \(celsius)"
+            resultLable.text = resultText
+        }
+        
+    }
+    
 }
 
